1cd14ce483e8921704be7329f98b079d64c44442
kkk <kuancarlos@mail.ru>
2025-01-17 20:09:41 +0500

    upd

--- src/common/config.py
+++ src/common/config.py
@@ -5,15 +5,15 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # common
-    app_name: str = "vanga-bot"
+    app_name: str = "tmp-bot"
     debug: bool = True
     
     # tg-bot
-    token: str = "7228537802:AAHc4PSiL4DTCIRKlgDM05vsWuKFW1sFTxk"
+    token: str = "token"
     admins: str = "00000000,000000001"
     
     # db
-    name_database: str = "vanga_bot_database.db"
+    name_database: str = "bot_database.db"
 
 
 config = Settings()
